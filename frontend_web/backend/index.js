c04aa313e19f8b236c02471584f05aa8c4d40ccf
lolmander3 <jolmanjhon@gmail.com>
2026-07-22 17:35:15 -0500

    fix: config IPs
    se cambiaron las ips

--- frontend_web/backend/index.js
+++ frontend_web/backend/index.js
@@ -15,12 +15,6 @@ app.set('port', port);
 
 const server = http.createServer(app);
 
-server.listen(port, host, () => {
-    console.log(`========================================`);
-    console.log(`🚀 Servidor corriendo en http://${host}:${port}`);
-    console.log(`📚 Swagger disponible en http://${host}:${port}/api-docs`);
-    console.log(`========================================`);
-});
 
 // ============================================
 // MANEJO DE ERRORES DEL SERVIDOR
