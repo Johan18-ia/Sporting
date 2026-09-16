e7d558167d83e5f570d5351494804367347a0ab4
Johan18-ia <johanestivenbernal25@gmail.com>
2026-09-16 17:29:23 -0500

    fix:teams mobile and tournaments.

--- frontend_mobile/src/config/api.ts
+++ frontend_mobile/src/config/api.ts
@@ -11,7 +11,7 @@ const getBaseUrl = () => {
     if (manualUrl) return manualUrl.replace(/\/$/, '');
 
     if (Platform.OS === 'android') {
-        return 'http://10.1.202.161:3000/api';
+        return 'http://10.1.202.105:3000/api';
     }
 
     return 'http://localhost:3000/api';
